ee1dac5bf7171cb18dc0d37d3f877c8eb653b4ab
Dandelion_God <dandeliongod95@gmail.com>
2019-06-26 01:12:17 +0300

    Exception in Sort. I need help

--- Solution/Project/MyList.cs
+++ Solution/Project/MyList.cs
@@ -6,13 +6,12 @@ using System.Threading.Tasks;
 
 namespace Project
 {
-	class MyList 
+	class MyList
 	{
 		private IntItem last;
 		private IntItem first;
-		private int count;  
-		
-		
+
+
 		public int this[int index]
 		{
 			get
@@ -27,54 +26,50 @@ namespace Project
 		}
 
 
-		public int Count
-		{
-			get
-			{
-				return count;
-			}
-		}
+		public bool IsEmpty
+		{ get => Count == 0; }
 
+		public int Count { get; set; }
 
 		public void Add(int item)
 		{
 			IntItem temp = new IntItem(item);
-			if(last != null)
+			if (last != null)
 			{
 				last.next = temp;
 				temp.prev = last;
 			}
-			if (count == 0)
+			if (IsEmpty)
 			{
 				first = temp;
 			}
 			last = temp;
-			count++;
+			Count++;
 		}
 
 
 		public void Remove(int index)
 		{
 			IntItem removed = first;
-			
+
 			//remove first
 			if (index == 0)
 			{
 				first = first.next;
 				first.prev = null;
-				count--;
+				Count--;
 				return;
 			}
-			
+
 			// remove last
-			if (index == count)
+			if (index == Count)
 			{
 				last = last.prev;
 				last.next = null;
-				count--;
+				Count--;
 				return;
 			}
-			
+
 			// remove from middle 
 			for (int i = 0; i < index; i++)
 			{
@@ -86,13 +81,7 @@ namespace Project
 			removed.next = null;
 			removed.prev = null;
 
-			count--;
-		}
-
-
-		public bool IsEmpty() // возвращает размер и проверяет пустой ли список, так как поля теперь приватные
-		{
-			return Count == 0;
+			Count--;
 		}
 
 
@@ -110,10 +99,50 @@ namespace Project
 		}
 
 
-		// TODO: Remove(5);
-		// TODO: ToString();
-		// TODO: Sort();
+		public void Sort()
+		{
+			IntItem i = first;
+			while (i != last.next)
+			{
+				IntItem j = i.next;
+				while (j !=last.next)
+				{
+					if (i.value > j.value)
+					{
+						IntItem temp = new IntItem(i.value);
+						//IntItem temp = i;
+						temp.next = i.next;
+						temp.prev = i.prev;
+						if (i.prev == null)
+						{
+							first = j;
+						}
+						if (j.next == null)
+						{
+							last = i;
+						}
+						i.next = j.next;
+						i.prev = j;
+						i.next.prev = i;
+						j.prev.next = j; //= temp.next
+						j.next = i;
+						j.prev = temp.prev;
+
+
+						//i.next = i.next.next;
+						//i.next.prev = temp;
+						//temp = i.prev;
+						//i.prev = i.next.prev;
+						//i.next.prev = temp;
+					}
+					j = j.next;
+				}
+				i = i.next;
+			}
+		}
 	}
 }
 
-
+		// TODO: Remove(5);
+		// TODO: ToString();
+		// TODO: Sort();
--- Solution/Project/Program.cs
+++ Solution/Project/Program.cs
@@ -11,21 +11,18 @@ namespace Project
 		{
 			MyList a = new MyList();
 			a.Add(2);
-			a.Add(-2);
-			a.Add(21);
-			a.Add(22);
-			a.Add(123);
+			a.Add(1);
+			a.Add(3);
 			a.Add(99);
 			a.Add(45);
-			a.Add(-7);
-			Console.Write(a.ToString());
-			a.Remove(4);
-			Console.Write(a.ToString());
-			a.Remove(0);
-			Console.Write(a.ToString());
+			a.Add(10000);
 			//a.Remove(123); // что если так
-            //int q = a[54]; // или так
-            //a.Add(12).Remove(2).Add(4); // хочу так
+			//int q = a[54]; // или так
+			//a.Add(12).Remove(2).Add(4); // хочу так
+			a.Sort();
+			Console.Write(a.ToString());
+
+
 			Console.ReadKey();
 		}
 	}
